21c498a3894f2aa79fe8a17bb32925208fb11863
Shaifali <sofiyachouhan1312@gmail.com>
2025-09-07 12:54:22 +0530

    Add files via upload

--- app/core/database.py
+++ app/core/database.py
@@ -2,11 +2,13 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+from app.core.config import settings
+
 # SQLite for local development
 SQLALCHEMY_DATABASE_URL = "sqlite:///./task_manager.db"
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    settings.SQLALCHEMY_DATABASE_URL
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
